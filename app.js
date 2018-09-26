e2b78f82f7190aa8df2dbe6ed338aa5426a9a2fd
Christophe Sauder <christophe.sauder@univ-lorraine.fr>
2018-09-26 11:40:04 +0200

    spacing

--- app.js
+++ app.js
@@ -17,6 +17,5 @@ app.use('/public', express.static('public'))
 // Partie paye ton serveur
 http.listen(process.env.PORT, console.log(`listening on port ${process.env.PORT}`))
 
-
 let stream = twitter(process.env.FILTER||'europe')
 stream.on('data', (data) => io.emit('popit', data))
